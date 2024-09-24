3a59b12aaf1aa3c853b9cba55b098de3fe1145d9
Kalthom Musa <kalthommusa12345@gmail.com>
2024-09-24 23:46:44 +0300

    update file

--- src/EditBlog.js
+++ src/EditBlog.js
@@ -1,22 +1,23 @@
 import { useState, useEffect } from "react";
 import { useHistory, useParams } from "react-router-dom";
+import useFetch from "./useFetch"; // Import the custom hook
 
 const EditBlog = () => {
   const { id } = useParams();
+  const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id); // Reuse useFetch
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [author, setAuthor] = useState('');
   const history = useHistory();
 
+  // Set form fields when blog data is fetched
   useEffect(() => {
-    fetch('http://localhost:8000/blogs/' + id)
-      .then(res => res.json())
-      .then(data => {
-        setTitle(data.title);
-        setBody(data.body);
-        setAuthor(data.author);
-      });
-  }, [id]);
+    if (blog) {
+      setTitle(blog.title);
+      setBody(blog.body);
+      setAuthor(blog.author);
+    }
+  }, [blog]); // This effect runs when blog data changes
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,38 +28,40 @@ const EditBlog = () => {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(updatedBlog)
     }).then(() => {
-      // history.push('/blogs/' + id);
       history.push('/');
     });
   };
 
   return (
     <div className="edit-blog">
-      <h2>Edit Blog</h2>
-      <form onSubmit={handleSubmit}>
-        <label>Blog title:</label>
-        <input 
-          type="text" 
-          required 
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
-        <label>Blog body:</label>
-        <textarea
-          required
-          value={body}
-          onChange={(e) => setBody(e.target.value)}
-        ></textarea>
-        <label>Blog author:</label>
-        <select
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        >
-          <option value="mario">mario</option>
-          <option value="yoshi">yoshi</option>
-        </select>
-        <button>Update Blog</button>
-      </form>
+      {isPending && <div>Loading...</div>}
+      {error && <div>{error}</div>}
+      {blog && (
+        <form onSubmit={handleSubmit}>
+          <label>Blog title:</label>
+          <input 
+            type="text" 
+            required 
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label>Blog body:</label>
+          <textarea
+            required
+            value={body}
+            onChange={(e) => setBody(e.target.value)}
+          ></textarea>
+          <label>Blog author:</label>
+          <select
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+          >
+            <option value="mario">mario</option>
+            <option value="yoshi">yoshi</option>
+          </select>
+          <button>Update Blog</button>
+        </form>
+      )}
     </div>
   );
 }
